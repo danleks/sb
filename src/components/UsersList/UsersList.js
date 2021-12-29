0979a8cf2841f84273769575549c90574cb21bd5
danleks <danleks@pop-os.localdomain>
2021-12-29 20:57:57 +0100

    setup the project

---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,12 @@
+import UsersListItem from 'components/UsersListItem/UsersListItem';
+import React from 'react';
+
+const UsersList = () => {
+    return (
+        <div>
+            <UsersListItem />
+        </div>
+    );
+};
+
+export default UsersList;
